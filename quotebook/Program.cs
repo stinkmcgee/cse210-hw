a3683d4fe7288a42c35790577601bc666ad1dde1
stinkmcgee <chr19003@byui.edu>
2023-05-05 23:44:14 -0600

    code along thing

---
+++ quotebook/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Source source = new Source ("None Were with Him", "http://www.churchofjesuschrist.org");
+
+Console.WriteLine(source.Stringify());
+
+Quote holland = new Quote("Elder Holland", "Because Jesus...");
+
+Console.WriteLine(holland.GetQuote());
+
+Board quoteboard = new Board();
+quoteboard.AddQuote(holland);
+quoteboard.ShowQuotes();
+quoteboard.FindQuotesByAuthor("holland");
+
+//_board.GetRandomQuote();
+
+Menu menu = new Menu(_board);
+_menu.Display();
