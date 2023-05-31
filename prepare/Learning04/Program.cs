fe93df437ed86dc2bec84f0e84a3a21f5fbf5d7e
stinkmcgee <chr19003@byui.edu>
2023-05-31 14:43:06 -0600

    Assignment assignment

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        //Console.WriteLine("Hello Learning04 World!");
+        Assignment a1 = new Assignment("Anakin Skywalker", "Multiplication");
+        Console.WriteLine(a1.GetSummary());
+        Console.WriteLine(" ");
+
+        MathAssignment a2 = new MathAssignment("Obi Wan Kenobi", "Fractions", "7.3", "8-19");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeWorkList());
+        Console.WriteLine(" ");
+
+        WritingAssignment a3 = new WritingAssignment("Ashoka Tano", "European History", "The Causes of World War II");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
     }
 }
--- prove/Develop03/scripture.cs
+++ prove/Develop03/scripture.cs
@@ -4,9 +4,9 @@ using System.Collections.Generic;
 public class Scripture{
     private List<Word> _words = new List<Word>();
 
-    Reference reference = new Reference(); //place refrence in parentheses
+    Reference reference = new Reference("John 3:16"); //place refrence in parentheses
 
-    private string _Scripture = " "; //place scripture in quotes. figure out how to let person manually place scripture and save it??
+    private string _Scripture = " For God so loved the world, that he gave his only Son, that whoever believes in him should not perish but have eternal life"; //place scripture in quotes. figure out how to let person manually place scripture and save it??
     public Scripture(string wholeText){
         string[] texts = wholeText.Split(" ");
         for (int i = 0; i < texts.Length; i++){
